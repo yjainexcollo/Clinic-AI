896e577fa2c6e53321c9a22e529688b2c82716ed
yjainexcollo <yashj@excollo.com>
2025-09-25 13:40:56 +0530

    New commit with changes of vital form and soap summary

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,6 +9,7 @@ import PersonalForm from "./components/PersonalForm";
 import Index from "./pages/Index";
 import SoapSummary from "./pages/SoapSummary";
 import VitalsForm from "./pages/VitalsForm";
+import TranscriptUpload from "./pages/TranscriptUpload";
 
 const App: React.FC = () => {
   return (
@@ -25,6 +26,7 @@ const App: React.FC = () => {
         <Route path="/intake/:patientId" element={<IntakePage />} />
         <Route path="/soap/:patientId/:visitId" element={<SoapSummary />} />
         <Route path="/vitals/:patientId/:visitId" element={<VitalsForm />} />
+        <Route path="/transcribe/:patientId/:visitId" element={<TranscriptUpload />} />
         <Route path="*" element={<Navigate to="/patient-registration" replace />} />
       </Routes>
     </Router>
